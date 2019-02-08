ed2f38c6c7d04eb015ca04a7509b943f48b798e4
Pomax <pomax@nihongoresources.com>
2019-02-07 21:35:02 -0800

    slightly nicer demo speeds

--- constants.js
+++ constants.js
@@ -4,8 +4,8 @@ const PRNG = new Random(SEED);
 const BOT_PLAY = true;
 const CONCEALED = true;
 const CLAIM_INTERVAL = 5000;
-const PLAY_INTERVAL = 0;
-const TURN_INTERVAL = 1000;
+const PLAY_INTERVAL = 100;
+const TURN_INTERVAL = 5000;
 const ARTIFICIAL_HUMAN_DELAY = 0;
 
 const LOW_TO_HIGH = (a,b) => { a = a.score; b = b.score; return a - b; };
