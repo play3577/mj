34790209193ba7d04e53f515606095c98f3c0272
Pomax <pomax@nihongoresources.com>
2019-03-28 14:10:38 -0700

    basic tileset

--- src/js/core/algorithm/tiles-needed.js
+++ src/js/core/algorithm/tiles-needed.js
@@ -1,6 +1,7 @@
 if (typeof process !== "undefined") {
   Pattern = require("./pattern.js");
   unroll = require('../utils/utils.js').unroll;
+  TileSet = require("./tileset.js");
 }
 
 
@@ -104,91 +105,91 @@ if (typeof process !== "undefined") { (function() {
     let tests = [
       {
         title: "winning mixed hand",
-        hand: [7,7,7, 15,15, 19,20,21, 22,23,24],
+        hand: new TileSet(7,7,7, 15,15, 19,20,21, 22,23,24),
         locked: [
-          [24,26,25]
+          new TileSet(24,26,25)
         ],
         win: true
       },
       {
         title: "winning hand with single hidden pung",
-        hand: [32,32,32],
+        hand: new TileSet(32,32,32),
         locked: [
-          [1,2,3],
-          [2,3,4],
-          [5,6,7],
-          [6,6]
+          new TileSet(1,2,3),
+          new TileSet(2,3,4),
+          new TileSet(5,6,7),
+          new TileSet(6,6),
         ],
         win: true
       },
       {
         title: "winning hand with single hidden pung, exposed kong",
-        hand: [32,32,32],
+        hand: new TileSet(32,32,32),
         locked: [
-          [26,24,25],
-          [1,2,3],
-          [30,30,30,30],
-          [31,31]
+          new TileSet(26,24,25),
+          new TileSet(1,2,3),
+          new TileSet(30,30,30,30),
+          new TileSet(31,31),
         ],
         win: true
       },
       {
         title: "winning hand, no tiles left in hand",
-        hand: [],
+        hand: new TileSet(),
         locked: [
-          [5,5,5,5],
-          [14,14,14],
-          [18,20,19],
-          [13,12,11],
-          [33,33]
+          new TileSet(5,5,5,5),
+          new TileSet(14,14,14),
+          new TileSet(18,20,19),
+          new TileSet(13,12,11),
+          new TileSet(33,33),
         ],
         win: true
       },
       {
         title: "winning hand with a kong, no tiles left in hand",
-        hand: [],
+        hand: new TileSet(),
         locked: [
-          [10,10,10],
-          [19,19,19],
-          [20,22,21],
-          [29,29,29,29],
-          [31,31]
+          new TileSet(10,10,10),
+          new TileSet(19,19,19),
+          new TileSet(20,22,21),
+          new TileSet(29,29,29,29),
+          new TileSet(31,31),
         ],
         win: true
       },
       {
         title: "winning hand, ambiguous pung/chow",
-        hand: [14,15,16,22,23,24,24,24],
+        hand: new TileSet(14,15,16,22,23,24,24,24),
         locked: [
-          [10,11,12],
-          [20,21,22]
+          new TileSet(10,11,12),
+          new TileSet(20,21,22),
         ],
         win: true
       },
       {
         title: "waiting hand, needs 18 or 30",
-        hand: [18,18,27,27,27,30,30,32,32,32],
+        hand: new TileSet(18,18,27,27,27,30,30,32,32,32),
         locked: [
-          [20,22,21]
+          new TileSet(20,22,21)
         ],
         win: false,
         need: [[18], [30]]
       },
       {
         title: "not a waiting hand, illegal win if 9 is claimed to form 8,9,10 (mixed suit)",
-        hand: [26,26,11,11,11,8,10],
+        hand: new TileSet(26,26,11,11,11,8,10),
         locked: [
-          [29,29,29],
-          [25,24,23]
+          new TileSet(29,29,29),
+          new TileSet(25,24,23),
         ],
         win: false,
         waiting: false
       },
       {
         title: "waiting on a gapped chow",
-        hand: [4,5,6, 9,10,11, 12,14, 15,15],
+        hand: new TileSet(4,5,6, 9,10,11, 12,14, 15,15),
         locked: [
-          [20,21,22]
+          new TileSet(20,21,22)
         ],
         win: false,
         waiting: true,
@@ -196,7 +197,7 @@ if (typeof process !== "undefined") { (function() {
       },
       {
         title: "can chow on a characters one (9)",
-        hand: [2,6,7,8,10,11,11,12,13,14,17,17,32],
+        hand: new TileSet(2,6,7,8,10,11,11,12,13,14,17,17,32),
         locked: [],
         win: false,
         waiting: false,
@@ -204,19 +205,19 @@ if (typeof process !== "undefined") { (function() {
       },
       {
         title: "holding 5,6,7,8 so any of {4,6,7,9} will do",
-        hand: [0,3,6, 9,14,15, 22,23,24,25, 3,9, 13],
-        locked: [],
+        hand: new TileSet(0,3,6, 9,14,15, 22,23,24,25, 3,9, 13),
+        locked: new TileSet(),
         win: false,
         waiting: false,
         want: [[21], [23], [24], [26]]
       },
       {
         title: "this really shouldn't be a win",
-        hand: [ 22, 24, 26, 27, 27 ],
+        hand: new TileSet( 22, 24, 26, 27, 27 ),
         locked: [
-          [9,10,11],
-          [0,1,2],
-          [23,24,25]
+          new TileSet(9,10,11),
+          new TileSet(0,1,2),
+          new TileSet(23,24,25),
         ],
         win: false,
         waiting: false
